7962538f6a46ae9946af04625c4701728351ff0f
Anvar Mohamed Aslam Sha <87097295+aslamshaw@users.noreply.github.com>
2025-03-21 11:06:13 +0100

    Delete 185. Department Top Three Salaries/185. Department Top Three Salaries.sql

--- 185. Department Top Three Salaries/185. Department Top Three Salaries.sql
+++
@@ -1,3 +0,0 @@
-WITH CTE AS (SELECT *, DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary DESC) AS ranking FROM employee)
-SELECT d.name AS Department, cte.name AS Employee, cte.salary FROM CTE 
-JOIN department AS d ON cte.departmentId = d.id WHERE ranking < 4;
